fbd10ab136d56d8b274b2a9f62c960d1dcc1b9bf
Matthew Ferrari <matthewferrari@matthews-mbp.mynetworksettings.com>
2024-12-12 14:45:28 -0500

    database fixes

--- db/elcproducts.sql
+++ db/elcproducts.sql
@@ -6,3 +6,14 @@ CREATE TABLE elcproducts (
 );
 INSERT INTO elcproducts (online_sku, name, price, description)
 VALUES ('001', 'Full Cooling Package', 1599.99, 'Full service package with custom cooling loops, pump, and liquid nitrogen included');
+
+INSERT INTO elcproducts (online_sku, name, price, description)
+VALUES('002', 'Loops Only', 699.99, 'Custom cooling loops, handmade for a perfect fit' );
+
+ALTER TABLE elcproducts ADD COLUMN image VARCHAR(255);
+
+UPDATE elcproducts
+SET image = 'loops.jpg'
+WHERE online_sku = '002';
+
+SELECT * from elcproducts
